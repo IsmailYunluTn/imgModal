29bf550e8d38477ed8f0caeef45b5a01f72a1826
IsmailYunluTn <ismailyunluisse@gmail.com>
2022-12-13 00:55:33 +0300

    Update

--- imgModal.js
+++ imgModal.js
@@ -1,21 +1,26 @@
 const modalHtml = "<div id='showImageModal'>"+
-                "<span class='close'>&times;</span>"+
-                "<img class='modal-content'>"+
-                "<div class='caption'></div>"+
-              "</div>";
+                    "<span class='close'>&times;</span>"+
+                    "<div class='modalBody'>"+
+                        "<div>"+
+                            "<img class='modal-content'>"+
+                            "<p class='caption'></p>"+
+                        "</div>"+
+                    "</div>"+
+                  "</div>";
 document.body.innerHTML += modalHtml;
 const modal = document.getElementById("showImageModal");
 const modalImg = modal.querySelector("img");
 const captionText = modal.querySelector(".caption");
 const span = modal.querySelector(".close");
 
-function imgModal(imgSelectors, caption = true) {
-    imgSelectors.forEach(e => {
-        const img = document.querySelector(e);
-        img.classList.add("modalImg");
-        img.onclick = function(){ 
+function imgModal(imgs, caption = true) {
+    imgs.forEach(e => {
+        e.classList.add("modalImg");
+        e.onclick = function(){ 
             modal.style.display = "block";
             modalImg.src = this.src;
+            modalImg.style.width = this.naturalWidth+"px";
+            modalImg.style.height = this.naturalHeight+"px";
             if(caption == true)
                 captionText.innerHTML = this.alt;
             else
